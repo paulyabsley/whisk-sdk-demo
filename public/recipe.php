16f6b7808c59eaefe1a1b6d2917ab94ec7f15b79
Paul Yabsley <yabsleyp@gmail.com>
2018-10-19 12:06:24 +0100

    Testing microformat layout

--- public/recipe.php
+++ public/recipe.php
@@ -8,22 +8,45 @@ $title = $recipe["title"];
 $description = $recipe["description"];
 
 include_once '_header.php';
-echo '<article class="h-recipe">';
-echo '<h1 class="p-name">' . $title . '</h1>';
-echo '<p class="p-summary">' . $description . '</p>';
-echo '<h2>Ingredients</h2>';
-echo '<ul>';
-foreach ($recipe["ingredients"] as $key => $ingredient) {
-	echo '<li class="p-ingredient"><span itemprop="recipeIngredient">' . $ingredient . '</span></li>';
-}
-echo '</ul>';
-echo '<button type="button" id="recipe-ingredients">Buy Ingredients</button>';
-echo '<h2>Directions</h2>';
-echo '<ol class="e-instructions">';
-foreach ($recipe["directions"] as $key => $direction) {
-	echo '<li>' . $direction . '</li>';
-}
-echo '</ol>';
-echo '</article>';
+// echo '<article class="h-recipe">';
+// echo '<h1 class="p-name fn">' . $title . '</h1>';
+// echo '<p class="p-summary">' . $description . '</p>';
+// echo '<h2>Ingredients</h2>';
+// echo '<ul>';
+// foreach ($recipe["ingredients"] as $key => $ingredient) {
+// 	echo '<li class="p-ingredient"><span itemprop="recipeIngredient">' . $ingredient . '</span></li>';
+// }
+// echo '</ul>';
+// echo '<button type="button" id="recipe-ingredients">Buy Ingredients</button>';
+// echo '<h2>Directions</h2>';
+// echo '<ol class="e-instructions">';
+// foreach ($recipe["directions"] as $key => $direction) {
+// 	echo '<li>' . $direction . '</li>';
+// }
+// echo '</ol>';
+// echo '</article>';
+
+?>
+
+<article class="h-recipe">
+  <h1 class="p-name">Bagels</h1>
+ 
+  <ul>
+    <li class="p-ingredient">Flour</li>
+    <li class="p-ingredient">Sugar</li>
+    <li class="p-ingredient">Yeast</li>
+  </ul>
+ 
+  <p>Takes <time class="dt-duration" datetime="1H">1 hour</time>,
+     serves <data class="p-yield" value="4">four people</data>.</p>
+ 
+  <div class="e-instructions">
+    <ol>
+      <li>Start by mixing all the ingredients together.</li>
+    </ol>
+  </div>
+</article><button type="button" id="recipe-ingredients">Buy Ingredients</button>
+
+<?php
 
 include_once '_footer.php';
